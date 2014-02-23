28587b5af0be3cea309f2715299bff444e4ba6f3
Andy M <andy.dude@gmail.com>
2014-02-23 13:46:28 -0600

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
+gem 'faker', '1.1.2'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,8 +21,22 @@ module SessionsHelper
   end
 
   def current_user
-    return @current_user ||= User.find_by(remember_token: 
+    return @current_user ||= User.find_by(:remember_token =>
       User.encrypt(cookies[:remember_token]))
 
   end
+
+  def current_user?(user)
+    return user == current_user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
 end
