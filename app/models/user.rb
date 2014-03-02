6220e8af01adeabfeee7ca1c7e2846092aa0c76f
Andy M <andy.dude@gmail.com>
2014-03-02 15:59:28 -0600

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
+  has_many :followed_users, :through => :relationships, :source => :followed 
+  has_many :reverse_relationships, :class_name => "Relationship", :foreign_key => "followed_id", :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   before_save { self.email.downcase!}
   before_create :create_remember_token
@@ -23,7 +27,19 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    return Micropost.where("user_id = ?", id)
+    return Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by(:followed_id => other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(:followed_id => other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(:followed_id => other_user.id).destroy
   end
   
   private
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,12 +12,34 @@ describe "Static pages" do
     it {should have_content("Sample App")}
     it {should have_title(full_title(''))}
     it {should_not have_title("| Home")}
-#    it "should have the content 'Sample App'" do
-#      expect(page).to have_content('Sample App')
-#    end
-#    it "should have the right title" do 
-#      expect(page).to have_title(base_title)
-#    end
+
+    describe "for signed-in users" do
+      let(:user) {FactoryGirl.create(:user)}
+      before do
+        FactoryGirl.create(:micropost, :user => user, :content => "Lorem")
+        FactoryGirl.create(:micropost, :user => user, :content => "Ipsum")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}"), :text => item.content
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) {FactoryGirl.create(:user)}
+        before do 
+          other_user.follow!(user)
+          visit root_path
+        end
+
+
+        it { should have_link("0 following", :href => following_user_path(user))}
+        it { should have_link("1 followers", :href => followers_user_path(user))}
+      end
+    end
   end
   
   describe "Help Page" do 
@@ -40,6 +62,7 @@ describe "Static pages" do
           expect(page).to have_selector("li##{item.id}", :text => item.content)
         end
       end
+
     end
 
   end
